a5f00fe37eb296bae86f4afc7fb9b316a5abe5e1
MartinaPerozzi <perozzimartina@gmail.com>
2023-02-14 11:07:08 +0100

    refactoring after lesson

--- js/script.js
+++ js/script.js
@@ -3,13 +3,11 @@ const app = Vue.createApp({
     data: function () {
         return {
             tasks: [
-                {
-                    text: "",
-                    done: false
-                },
-
             ],
-            newTask: {},
+            newTask: {
+                text: "",
+                done: false
+            },
             error: false,
 
         }
@@ -18,20 +16,20 @@ const app = Vue.createApp({
     methods: {
 
         addTask() {
+            // Copia dell'oggetto newTask(Copy)
+            // ...this.newTask SI COPIA CON LO SPREAD in modo più veloce e senza dover rimettere dentro le proprietà
             const newTask = {
                 text: this.newTask.text,
                 done: false,
             };
-            // Controllo numero lettere
+            // Controllo numero lettere permesso
             console.log(this.newTask)
             if (this.newTask.text.length >= 4) {
                 this.tasks.push(newTask);
+                // Controllo che il numero di caratteri sia superiore a 4
                 this.error = false;
-                // prova
-                const input = document.querySelector(".form-input").value;
-                input.innerHTML = "";
-                console.log(input)
-
+                // Ripulire l'input dopo aver inviato la Task
+                this.newTask.text = "";
             } else {
                 this.error = true;
             }
@@ -43,14 +41,17 @@ const app = Vue.createApp({
             this.tasks.splice(i, 1);
         },
         // Prova Sbarramento- da capire
-        doneTask() {
-            let doneTaskEl = this.newTask.done = true;
-            console.log(doneTaskEl);
+        // doneTask() {
+        //     let doneTaskEl = this.newTask.done = true;
+        //     console.log(doneTaskEl);
+        // }
+
+        doneTask(index) {
+            console.log(index);
+            this.tasks[index].done = !this.tasks[index].done;
         }
 
-
     }
-
 
 });
 app.mount("#root");
